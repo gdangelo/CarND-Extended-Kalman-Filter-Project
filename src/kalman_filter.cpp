82107b0219266da463d2d83654be58497987a514
gdangelo <gregory@gdangelo.fr>
2018-02-06 14:51:30 -0500

    feat: Implement Kalman Predict & Update methods

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,9 +1,10 @@
 #include "kalman_filter.h"
+#include "tools.h"
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-// Please note that the Eigen library does not initialize 
+// Please note that the Eigen library does not initialize
 // VectorXd or MatrixXd objects with zeros upon creation.
 
 KalmanFilter::KalmanFilter() {}
@@ -21,22 +22,45 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+  // Predict the state after delta_T
+  x_ = F_*x_;
+  // Update the State Covariance Matrix P (uncertainty)
+  VectorXd Ft = F_.transpose();
+  P_ = F_*P_*Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+  VectorXd y = z - H_*x_;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_*P_*Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K = P_*Ht*Si;
+  MatrixXd I = MatrixXd::Identity(4, 4);
+
+  // Update
+  x_ = x_ + K*y;
+  P_ = (I - K*H_)*P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+  // Use the equations that map the predicted location
+  // from Cartesian coordinates to polar coordinates
+  VectorXd hx(3);
+  hx << sqrt(x_(0)*x_(0) + x_(1)*x_(1)),
+                 atan(x_(1)/x_(0)),
+                 (x_(0)*x_(2) + x_(1)*x_(3))/hx(0);
+
+  VectorXd y = z - hx;
+
+  Tools tools;
+  MatrixXd Hj = tools.CalculateJacobian(x_);
+  MatrixXd Ht = Hj.transpose();
+  MatrixXd S = Hj*P_*Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K = P_*Ht*Si;
+  MatrixXd I = MatrixXd::Identity(4, 4);
+
+  // Update
+  x_ = x_ + K*y;
+  P_ = (I - K*Hj)*P_;
 }
